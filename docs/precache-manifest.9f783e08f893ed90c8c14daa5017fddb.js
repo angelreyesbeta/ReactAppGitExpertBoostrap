364171eaea43b42c490de7bdb601023ab3241b12
Angel reyes <angelreyesbeta@hotmail.com>
2020-09-08 18:05:32 -0500

    docs Agregado

---
+++ docs/precache-manifest.9f783e08f893ed90c8c14daa5017fddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116c5193e80d1ededa3c03ce3548f7fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c2330388b659961d55",
+    "url": "/static/css/main.4ce7821a.chunk.css"
+  },
+  {
+    "revision": "d4482acf65c930b839fc",
+    "url": "/static/js/2.ec82ce75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec82ce75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c2330388b659961d55",
+    "url": "/static/js/main.65223db2.chunk.js"
+  },
+  {
+    "revision": "6f7451d029965e133f77",
+    "url": "/static/js/runtime-main.135d38b9.js"
+  }
+]);
